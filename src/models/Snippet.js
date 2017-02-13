c1adc49c1efa1d781d37b4fa0d1e63d4920ac371
“neilburd” <neilburd@gmail.com>
2017-02-13 17:19:01 -0500

    switching branches and merging

--- src/models/Snippet.js
+++ src/models/Snippet.js
@@ -1,20 +1,23 @@
 import axios from 'axios'
+//let URL = 'https://intense-plains-13482.herokuapp.com'
+let URL = 'https://localhost:3000'
 
 class SnippetModel {
+
   static all(){
-    let request = axios.get("https://intense-plains-13482.herokuapp.com/snippets")
+    let request = axios.get(`${URL}/snippets`)
     return request
   }
   static create(snippet) {
-    let request = axios.post("https://intense-plains-13482.herokuapp.com/snippets", snippet)
+    let request = axios.post(`${URL}/snippets`, snippet)
     return request
   }
   static deleteSnippet(snippet){
-    let request = axios.delete(`https://intense-plains-13482.herokuapp.com/snippets/${snippet.id}`)
+    let request = axios.delete(`${URL}/snippets/${snippet.id}`)
     return request
   }
     static update(snippet){
-      let request = axios.put(`https://intense-plains-13482.herokuapp.com/snippets/${snippet.id}`, {code: snippet.code})
+      let request = axios.put(`${URL}/snippets/${snippet.id}`, {code: snippet.code})
       return request
     }
   }
