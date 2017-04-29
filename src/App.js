43759edaa967c9a620809590f3d3e88ac18c4162
neilburd <neilburd@gmail.com>
2017-04-29 12:08:09 -0400

    changed to basic react app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import Nav from './components/Nav.js'
 import SnippetsContainer from './containers/SnippetsContainer.js';
-// import './App.css';
 
 class App extends Component {
   render() {
